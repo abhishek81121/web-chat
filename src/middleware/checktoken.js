0ca0f2e9b89d3264bab3165003513f770fb328b2
ABHISHEK KUMAR SINGH <129748757+abhishek81121@users.noreply.github.com>
2023-06-10 10:09:39 +0530

    written basic middleware

--- src/middleware/checktoken.js
+++ src/middleware/checktoken.js
@@ -1,2 +1,13 @@
 // middleware for the jwt
-export default function checktoken(req, res, next) {}
+export default function checktoken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
